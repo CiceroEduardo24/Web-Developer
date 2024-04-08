a87a4c120673ba84bf1d18504363da7b5a1db2d5
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-04-08 19:35:58 -0300

    add database connection

---
+++ 7-Node.js/class-05-authentication/src/databases/sqlite3/migrations/index.ts
@@ -0,0 +1,12 @@
+import { sqliteConnection } from "..";
+import { tableUsers } from "./tableUsers";
+
+export async function runMigrations() {
+  const schemas = [tableUsers].join("");
+
+  sqliteConnection()
+    .then((db) => db.exec(schemas))
+    .catch((error) => console.log(error));
+
+  return;
+}
--- 7-Node.js/class-05-authentication/src/server.ts
+++ 7-Node.js/class-05-authentication/src/server.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import { routers } from "./routes";
 import { appErrors } from "./errors/appErrors";
 import { pageNotFound } from "./errors/pageNotFound";
+import { sqliteConnection } from "./databases/sqlite3";
+import { runMigrations } from "./databases/sqlite3/migrations";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,3 +18,9 @@ app.use(appErrors);
 app.listen(PORT, () => {
   console.log(`Server is running on port: http://localhost:${PORT}/`);
 });
+
+sqliteConnection()
+  .then(() => console.log("Database is connected..."))
+  .catch((error) => console.error("Database isn't connected -", error));
+
+runMigrations();
