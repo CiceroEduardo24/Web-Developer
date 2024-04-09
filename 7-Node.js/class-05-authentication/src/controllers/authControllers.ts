f83c459bf13eca6b40aa2957dc4dfe4232582f5c
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-04-09 20:44:43 -0300

    add json web tokenjwt token and respose cookie

--- 7-Node.js/class-05-authentication/src/controllers/authControllers.ts
+++ 7-Node.js/class-05-authentication/src/controllers/authControllers.ts
@@ -1,9 +1,50 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
+import { userRepository } from "../repositories/userRepository";
+import { compare } from "bcrypt";
+import { z } from "zod";
+import { sign } from "jsonwebtoken";
 
 export const authControllers = {
   async login(req: Request, res: Response, next: NextFunction) {
     try {
-      return res.status(200).json({ message: "User logged in!" });
+      const userSchema = z
+        .object({
+          email: z
+            .string({
+              invalid_type_error: "Somente texto!",
+            })
+            .email({ message: "Email com formato inválido!" })
+            .max(255, "Tamanho máximo atingido por email!"),
+
+          password: z
+            .string({
+              invalid_type_error: "Para a senha use o tipo string!",
+            })
+            .max(255, "Tamanho máximo atingido por senha!"),
+        })
+        .strict();
+      const { email, password } = userSchema.parse(req.body);
+
+      const user = await userRepository.getbyEmail(email);
+      if (!user) throw res.status(401).json({ message: "Email invalid!" });
+
+      const passwordCheck = await compare(password, user.password);
+      if (!passwordCheck) {
+        throw res.status(401).json({ message: "Password invalid!" });
+      }
+
+      const token = sign({ id: user.id }, process.env.SECRET_TOKEN, {
+        expiresIn: process.env.EXPIRESIN_TOKEN,
+      });
+
+      res.cookie(process.env.KEY_TOKEN, token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        maxAge: 1000 * 60 * 15,
+      });
+
+      return res.status(200).json({ message: "User logged in!", token });
     } catch (error) {
       return next(error);
     }
